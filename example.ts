adbd63ff8a27954f749df505e8d439c451a2a4f3
CJ <cj@specialblend.net>
2020-07-31 15:56:39 -0500

    update docs

--- example.ts
+++ example.ts
@@ -1,33 +1,53 @@
+import { GenericException } from '@specialblend/exceptional';
+
 import { Logger, LogLevel } from './src/Logger';
 
 (function main() {
-    const appLogger = new Logger({ name: 'myApp', namespace: 'app', level: LogLevel.silly });
-    appLogger.silly({
-        message: 'you are awesome.',
-        foo: 'test foo',
+
+    const logger = new Logger({ name: 'myApp', namespace: 'app', level: LogLevel.SILLY });
+    logger.type('panic').fatal({
+        message: '.',
+        foo: 'sedxfrghj',
     });
-    appLogger.trace({
-        message: 'I talk too much.',
-        bar: 'test bar',
-    });
-    appLogger.info({
-        message: 'I am working just fine.',
+
+    logger.type('unhandled_error').error({
+        message: 'i caught an unhandled error.',
         baz: 'test baz',
+        err: new Error('oops!'),
     });
-    appLogger.debug({
+
+    logger.type('creating_product').trace({
+        message: 'i am about to create a new product by calling the foo-products api.',
+        url: 'https://foo-api.example.com/api/products',
+        product: {
+            name: 'test product #1',
+            sku: '12345',
+        },
+    });
+    logger.debug({
         message: 'Here is more information about stuff when stuff broke.',
         foo: 'test foo',
     });
-    appLogger.warn({
+    logger.warn({
         message: 'This shouldn\'t be happening. You should be ashamed of yourself.',
         bar: 'test bar',
     });
-    appLogger.error({
-        message: 'I am not quite dying, but something really bad happened. Fix me.',
-        baz: 'test baz',
+    logger.info({
+        message: 'ok. i am now connected to postgres.',
+        host: 'https://postgres-backend.example.com',
     });
-    appLogger.fatal({
-        message: 'I am dying because I can\'t handle this error.',
-        faz: 'test faz',
+    logger.type('random_thought').silly({
+        message: 'you are awesome.',
+        foo: 'test foo',
     });
+
+    class MissingTokenEx extends GenericException {}
+
+    logger.exception(
+        new MissingTokenEx(
+            'request is missing auth token. please review'
+        ),
+        LogLevel.ERROR
+    );
+
 }());
